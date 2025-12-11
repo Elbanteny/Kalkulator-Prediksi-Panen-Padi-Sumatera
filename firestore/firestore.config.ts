d5922b49e3da715027e5b372a424d935cd489594
Elbanteny <m.amiryan.p@gmail.com>
2025-12-11 17:15:22 +0700

    Saving api_key

--- firestore/firestore.config.ts
+++ firestore/firestore.config.ts
@@ -1,21 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const requiredEnvVars = [
-  "NEXT_PUBLIC_FIRESTORE_API_KEY",
-  "NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN",
-  "NEXT_PUBLIC_FIREBASE_PROJECT_ID",
-  "NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET",
-  "NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID",
-  "NEXT_PUBLIC_FIREBASE_APP_ID",
-];
-
-for (const envVar of requiredEnvVars) {
-  if (!process.env[envVar]) {
-    throw new Error(`Missing required environment variable: ${envVar}`);
-  }
-}
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIRESTORE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
